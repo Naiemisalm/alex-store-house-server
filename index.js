234c9fdb85706e57363a7a9701cc430b7312aee0
Naiemisalm <nayemislam30964@gmail.com>
2022-05-01 15:52:14 +0600

    alex server basic implementation

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 const express = require('express');
-const cros = require('cros');
-// echo "# warehouse-management-server-side-Naiemisalm" >> README.md
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abtli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("Alex-store").collection("product");
+//   console.log('db connect')
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+async function run(){
+  try{
+
+  }
+  finally{
+    
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('Running Alex Store House');
+});
+
+app.listen(port, () => {
+  console.log('Listening to port', port);
+})
