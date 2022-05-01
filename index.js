f430b1aa222b447f2100bb5e64e062f9d85c6042
Naiemisalm <nayemislam30964@gmail.com>
2022-05-01 21:52:00 +0600

    mongodb api load

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abtli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://dbuser1:79lwQWaSnq3yURPh@cluster0.abtli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // client.connect(err => {
 //   const collection = client.db("Alex-store").collection("product");
@@ -21,14 +21,22 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 // });
 
 
-async function run(){
-  try{
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db("Alex-store").collection("product");
+    app.get('/product', async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  }
+  finally {
 
   }
-  finally{
-    
-  }
 }
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('Running Alex Store House');
